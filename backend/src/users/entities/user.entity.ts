29765d9769d921537a8e62d00e1d92ecab863c52
Carlos Bolívar <cebemind@hotmail.com>
2026-01-12 15:17:19 -0400

    chore: convert to monorepo workspace

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Note } from '../../notes/entities/note.entity';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  passwordHash: string;
+
+  @OneToMany(() => Note, (note) => note.user)
+  notes: Note[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
